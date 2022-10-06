c78bc9476e53b411753a67cc78041922fbb88381
zamahsyari <zmachmobile@gmail.com>
2022-10-06 20:05:00 +0700

    fix redis connect

--- helper/redis.js
+++ helper/redis.js
@@ -1,17 +1,31 @@
 require('dotenv').config()
 var redis = require('redis')
 var client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PASSWORD
+    legacyMode: true,
+    url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}`
 })
 const { cli } = require('winston/lib/winston/config')
 
 module.exports = {
-    init: async () => { await client.connect() },
+    init: async () => {
+        try{
+            await client.connect()
+        }catch(e){
+            throw e
+        }
+    },
     get: async (key) => {
-        return await client.get(key)
+        try{
+            return await client.get(key)
+        }catch(e){
+            throw e
+        }
     },
     set: async (key, value) => {
-        return await client.set(key, value)
+        try{
+            return await client.set(key, value)
+        }catch(e){
+            throw e
+        }
     }
 }
